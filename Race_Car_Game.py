7ab0ac7de93fc459b32d5fa8de708eefa1da18be
Chase Goodband <cgoodband1@gmail.com>
2020-12-11 21:12:35 -0500

    Feactures added
    features added:
    main screen before entering game
    pausing game by pressing "p"
    better crash function that gives you buttons to play again or quit.

--- Race_Car_Game.py
+++ Race_Car_Game.py
@@ -9,8 +9,10 @@ display_height = 600
 
 black = (0,0,0)
 white = (250,250,250)
-red = (250,0,0)
-green = (0,250,0)
+red = (200,0,0)
+bright_red = (255,0,0)
+green = (0,200,0)
+bright_green = (0,255,0)
 blue = (0,0,250)
 
 car_width = 73
@@ -21,6 +23,8 @@ pygame.display.set_caption('A Bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car.png')
+pause = False
+
 
 def things_dodged(count):
 	font = pygame.font.SysFont(None, 25)
@@ -36,7 +40,7 @@ def car(x,y):
 	gameDisplay.blit(carImg, (x,y))
 
 def text_objects(text, font):
-	textSurface = font.render(text, True, red)
+	textSurface = font.render(text, True, black)
 	return textSurface, textSurface.get_rect()
 
 
@@ -53,15 +57,92 @@ def message_display(text):
 	game_loop()
 
 def crash():
-	message_display('You Crashed!')
+	
+	largeText = pygame.font.Font('freesansbold.ttf', 115)
+	TextSurf, TextRect = text_objects("You Crashed", largeText)
+	TextRect.center = ((display_width/2),(display_height/2))
+	gameDisplay.blit(TextSurf, TextRect)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+		#gameDisplay.fill(white)
+
+		
+		button("Play Again",150,450,110,50,green,bright_green, game_loop)
+		button("QUIT",550,450,100,50,red,bright_red,quitgame)
+
+		pygame.display.update()
+		clock.tick(15)
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+	mouse = pygame.mouse.get_pos()
+	click = pygame.mouse.get_pressed()
+	if x+w > mouse[0] > x and y + h > mouse[1] > y:
+		pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
+		if click[0] == 1 and action != None:
+			action()
+	else:
+		pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
+
+		smalltext = pygame.font.Font("freesansbold.ttf", 20)
+		TextSurf, TextRect = text_objects(msg, smalltext)
+		TextRect.center = ((x + (w/2)), (y + (h/2)))
+		gameDisplay.blit(TextSurf, TextRect)
+
+def quitgame():
+	pygame.quit()
+	quit()
+
+def unpause():
+	global pause
+	pause = False
+
+def Paused():
+	largeText = pygame.font.Font('freesansbold.ttf', 115)
+	TextSurf, TextRect = text_objects("Paused", largeText)
+	TextRect.center = ((display_width/2),(display_height/2))
+	gameDisplay.blit(TextSurf, TextRect)
+	while pause:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+		#gameDisplay.fill(white)
+
+		
+		button("Continue",150,450,100,50,green,bright_green, unpause)
+		button("QUIT",550,450,100,50,red,bright_red,quitgame)
+
+		pygame.display.update()
+		clock.tick(15)
 
 
+def game_intro():
+	intro = True
 
+	while intro:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+		gameDisplay.fill(white)
+		largeText = pygame.font.Font('freesansbold.ttf', 115)
+		TextSurf, TextRect = text_objects("A bit Racey", largeText)
+		TextRect.center = ((display_width/2),(display_height/2))
+		gameDisplay.blit(TextSurf, TextRect)
+		
+		button("GO!",150,450,100,50,green,bright_green, game_loop)
+		button("QUIT",550,450,100,50,red,bright_red,quitgame)
 
+		pygame.display.update()
+		clock.tick(15)
 
 
 
 def game_loop():
+	global pause
 	x = (display_width *0.4)
 	y = (display_height *0.7)
 
@@ -84,8 +165,12 @@ def game_loop():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
 					x_change = -12
-				elif event.key == pygame.K_RIGHT:
+				if event.key == pygame.K_RIGHT:
 					x_change = 12
+				if event.key == pygame.K_p:
+					pause = True
+					Paused()
+
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 					x_change = 0
@@ -104,6 +189,7 @@ def game_loop():
 			thing_starty = 0 - thing_height
 			thing_startx = random.randrange(0,display_width)
 			dodged += 1
+
 			thing_speed += 1
 			#thing_width += (dodged * 1.2)
 
@@ -120,6 +206,7 @@ def game_loop():
 		pygame.display.update()
 		clock.tick(60)
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
